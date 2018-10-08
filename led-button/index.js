33f282f548f7e5fb7ed66a5ea7e99ebfa3112ae4
Justas Palumickas <jpalumickas@gmail.com>
2018-10-09 00:29:10 +0300

    More updates in LED examples

--- led-button/index.js
+++ led-button/index.js
@@ -1,19 +1,20 @@
-var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
-var LED = new Gpio(18, 'out'); //use GPIO pin 18 as output
-var pushButton = new Gpio(23, 'in', 'both'); //use GPIO pin 23 as input, and 'both' button presses, and releases should be handled
+const Gpio = require('onoff').Gpio;
+const LED = new Gpio(4, 'out');
+const pushButton = new Gpio(17, 'in', 'both');
 
-pushButton.watch(function (err, value) { //Watch for hardware interrupts on pushButton GPIO, specify callback function
-  if (err) { //if an error
-    console.error('There was an error', err); //output error message to console
-  return;
+pushButton.watch(function (err, value) {
+  if (err) {
+    console.error('There was an error', err);
+    return;
   }
-  LED.writeSync(value); //turn LED on or off depending on the button state (0 or 1)
+
+  LED.writeSync(value);
 });
 
-function unexportOnClose() { //function to run when exiting program
-  LED.writeSync(0); // Turn LED off
-  LED.unexport(); // Unexport LED GPIO to free resources
-  pushButton.unexport(); // Unexport Button GPIO to free resources
+const unexportOnClose = () => {
+  LED.writeSync(0);
+  LED.unexport();
+  pushButton.unexport();
 };
 
-process.on('SIGINT', unexportOnClose); //function to run when user closes using ctrl+c
+process.on('SIGINT', unexportOnClose);
